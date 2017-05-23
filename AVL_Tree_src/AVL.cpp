17c121e44ea1b052cb555f9d5baa0769ed6876b0
André Cascais <a_cascais@hotmail.com>
2017-05-23 20:15:47 +0100

    Insertion covered all cases (still needs some testing)

--- AVL_Tree_src/AVL.cpp
+++ AVL_Tree_src/AVL.cpp
@@ -90,8 +90,8 @@ void Node::set_height(int h) {
 }
 
 void Node::update_height() {
-    int height_left = (this->get_left() == NULL ? 0 : this->get_left()->get_height());
-    int height_right = (this->get_right() == NULL ? 0 : this->get_right()->get_height());
+    int height_left = (this->get_left() == NULL ? -1 : this->get_left()->get_height());
+    int height_right = (this->get_right() == NULL ? -1 : this->get_right()->get_height());
     height = 1 + max(height_left, height_right);
 }
 
@@ -191,16 +191,69 @@ void AVLTree::traverse_backwards(Node* node) {
             Node* new_root = node->get_left();
             new_root->set_father(node->get_father());
 
-            Node* temp = node->get_right();
+            Node* temp = new_root->get_right();
 
             new_root->set_right(node);
             node->set_left(temp);
             node->update_height();
             node = new_root;
-        } else { // left -> right case3 @ example
-
+        } 
+		else { // left -> right case3 @ example
+			printf("case3\n");
+            Node* new_root = node->get_left()->get_right();
+            new_root->set_father(node->get_father());
+            
+            Node* temp_left = new_root->get_left();
+            Node* temp_right = new_root->get_right();
+            
+            new_root->set_left(node->get_left());
+            new_root->set_right(node);
+            
+            new_root->get_left()->set_right(temp_left);
+            new_root->get_right()->set_left(temp_right);
+            
+            node->update_height();
+            new_root->update_height();
+            node = new_root;
+		
         }
-    } else if (diff > 1) { // right deeper
+    }
+    else if (diff > 1) { // right deeper
+        
+        int right_diff = node->get_right()->get_height_diff();
+        
+        if (right_diff < 0) { // right->left case4
+            printf("case4\n");
+            Node* new_root  = node->get_right()->get_left();
+            new_root->set_father(node->get_father());
+            
+            Node* temp_left = new_root->get_left();
+            Node* temp_right = new_root->get_right();
+            
+            new_root->set_left(node);
+            new_root->set_right(node->get_right());
+            
+            new_root->get_left()->set_right(temp_left);
+            new_root->get_right()->set_left(temp_right);
+           
+            node->update_height();
+            new_root->update_height();
+            node = new_root;
+            
+        }
+        else { // right->right case2
+            printf("case2\n");
+            Node* new_root = node->get_right();
+            new_root->set_father(node->get_father());
+            
+            Node* temp = new_root->get_left();
+            
+            new_root->set_left(node);
+            node->set_right(temp);
+            node->update_height();
+            node = new_root;
+            
+        }
     }
 
     if (node->get_father() == NULL) {
@@ -255,9 +308,16 @@ void AVLTree::iterate_tree() {
 int main(int arc, char** argv) {
     //AVLTree *a = new AVLTree;
     AVLTree* t = new AVLTree();
-    t->add_value(3);
     t->add_value(2);
-    t->add_value(1);
+    t->add_value(78);
+    t->add_value(49);
+    t->add_value(50);
+    t->add_value(21);
+    t->add_value(34);
+    t->add_value(36);
+    t->add_value(81);
+    t->add_value(14);
+    t->add_value(70);
     t->iterate_tree();
     return 0;
 }
