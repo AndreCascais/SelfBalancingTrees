08df54764ce661650b23ae731d953a6ffd8de6ef
Raul Ferreira <raulmendesferreira@gmail.com>
2017-07-24 01:23:12 +0100

    Function parameters should be named.

--- AVL_Tree_src/AVL.cpp
+++ AVL_Tree_src/AVL.cpp
@@ -24,14 +24,14 @@ public:
     void update_height();
     int get_height_diff();
     T get_value();
-    void set_left(Node<T>*);
+    void set_left(Node<T>* n);
     Node<T>* get_left();
-    void set_right(Node<T>*);
+    void set_right(Node<T>* n);
     Node<T>* get_right();
-    void set_father(Node<T>*);
+    void set_father(Node<T>* n);
     Node<T>* get_father();
-    void set_son(Node<T>*);
-    void delete_son(Node<T>*);
+    void set_son(Node<T>* n);
+    void delete_son(Node<T>* n);
 
     Node<T>* get_min();
     Node<T>* get_max();
@@ -52,22 +52,22 @@ public:
     ~AVLTree();
     void iterate_tree();
     void destroy_tree();
-    void add_value(T);
-    void remove_value(T);
-    Node<T>* lookup(T);
+    void add_value(T v);
+    void remove_value(T v);
+    Node<T>* lookup(T v);
     double get_ratio();
 
 private:
-    void destroy_tree(Node<T>*);
+    void destroy_tree(Node<T>* node);
     void delete_node(Node<T>*);
-    Node<T>* find_value(Node<T>*, T);
-    void rotate_right(Node<T>*);
-    void rotate_left(Node<T>*);
-    Node<T>* add_value(Node<T>*, T);
-    Node<T>* remove_node(Node<T>*);
-    void traverse_backwards(Node<T>*);
-    int get_n_nodes(Node<T>*);
-    int get_height(Node<T>*);
+    Node<T>* find_value(Node<T>* node, T v);
+    void rotate_right(Node<T>* nodex_x);
+    void rotate_left(Node<T>* nodex_x);
+    Node<T>* add_value(Node<T>* node, T v);
+    Node<T>* remove_node(Node<T>* node);
+    void traverse_backwards(Node<T>* node);
+    int get_n_nodes(Node<T>* node);
+    int get_height(Node<T>* node);
 
     Node<T>* root;
 };
